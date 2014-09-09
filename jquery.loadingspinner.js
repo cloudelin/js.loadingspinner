18a6dee22c5e52078e92fcb33d29bb0017320cf2
cloudelin <maxcloude@gmail.com>
2014-09-09 11:51:54 +0800

    first commit

---
+++ jquery.loadingspinner.js
@@ -0,0 +1,35 @@
+/**
+ * @author Cloude
+ */
+
+(function($) {
+	
+	var default_msg = "Loading ...";
+	var maindiv = $('<div/>').addClass('loading-spinner-maindiv');
+	var imgdiv = $('<div><i class="fa fa-spinner fa-spin"></i></div>').addClass('loading-spinner-imgdiv');
+	var messagediv = $('<div/>').addClass('loading-spinner-messagediv');
+	var messagespan = $('<span/>').addClass('loading-spinner-messagespan');
+	
+	function Spinner($this, options) {
+        this.$this = $this;
+        this.options = options;
+    };
+    
+    Spinner.prototype.loading = function () {
+        messagespan.text(default_msg);
+        
+        $('body').append(maindiv.append(imgdiv).append(messagediv.append(messagespan)));
+        
+        var margin_l = -(maindiv.width() / 2);
+        maindiv.css('margin-left', margin_l + 'px');
+     };
+     
+     Spinner.prototype.done = function() {
+    	 setTimeout(function(){maindiv.remove();}, 800);
+     }
+	
+	$.spinner = function( options ) {
+		return new Spinner(this, options);
+	};
+    
+}(jQuery));
